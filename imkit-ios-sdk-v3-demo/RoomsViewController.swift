dac3d0f5fb2e6a48457d4aaa3fee9126b512096e
Howard Sun <howardsun@csie.io>
2021-02-10 22:50:51 +0800

    Update imkit to 0.320.0

--- imkit-ios-sdk-v3-demo/RoomsViewController.swift
+++ imkit-ios-sdk-v3-demo/RoomsViewController.swift
@@ -8,34 +8,3 @@
 
 import Foundation
 import IMKit
-
-class RoomsViewController: IMRoomsViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "登出", style: .plain, target: self, action: #selector(logout))
-    }
-    
-    @objc func logout() {
-        IMUnsubscribeTask().perform().done { _ in
-            IMSocketManager.shared.disconnect()
-            IMKit.clear()
-            self.dismiss(animated: true, completion: nil)
-        }.catch { error in
-            print(error)
-        }
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let element = viewModel.sections[indexPath.section].elements[indexPath.row]
-        if let room = element.base as? IMRoom {
-            guard let navigationController = navigationController else { return }
-            let vc = MessagesViewController(roomID: room.id)
-            vc.hidesBottomBarWhenPushed = true
-            vc.navigationItem.title = room.name
-            navigationController.pushViewController(vc, animated: true)
-        } else {
-            super.collectionView(collectionView, didSelectItemAt: indexPath)
-        }
-    }
-}
