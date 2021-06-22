17b77bac263286f9f46855215065f9b07cb636e9
sean <shonshon7@gmail.com>
2021-06-22 22:49:34 +0800

    [add] ScenarioIntroViewController

---
+++ imkit-ios-sdk-v3-demo/ScenarioIntroViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ScenarioIntroViewController.swift
+//  imkit-ios-sdk-v3-demo
+//
+//  Created by Sean on 2021/6/22.
+//  Copyright © 2021 Howard Sun. All rights reserved.
+//
+
+import UIKit
+
+class ScenarioIntroViewController: UIViewController {
+
+    @IBOutlet weak var startButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        startButton.layer.cornerRadius = startButton.bounds.height / 2.0
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
