19a112528863d17794551d5f53e98a692c6aed85
sean <shonshon7@gmail.com>
2021-06-24 00:53:01 +0800

    [add] TradingPlatformProductViewController

---
+++ imkit-ios-sdk-v3-demo/TradingPlatformProductViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TradingPlatformProductViewController.swift
+//  imkit-ios-sdk-v3-demo
+//
+//  Created by Sean on 2021/6/23.
+//  Copyright © 2021 Howard Sun. All rights reserved.
+//
+
+import UIKit
+
+class TradingPlatformProductViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
