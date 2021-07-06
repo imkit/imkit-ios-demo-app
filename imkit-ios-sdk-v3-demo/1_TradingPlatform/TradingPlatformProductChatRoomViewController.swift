4fad8b7717c8890499dc4e456aa9fc5f377f0d05
sean <shonshon7@gmail.com>
2021-07-06 20:48:44 +0800

    [add] create multi vcs needed

---
+++ imkit-ios-sdk-v3-demo/1_TradingPlatform/TradingPlatformProductChatRoomViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TradingPlatformProductChatRoomViewController.swift
+//  imkit-ios-sdk-v3-demo
+//
+//  Created by Sean on 2021/7/6.
+//  Copyright © 2021 Howard Sun. All rights reserved.
+//
+
+import Foundation
+import IMKit
+
+class TradingPlatformProductChatRoomViewController: IMChatRoomViewController {
+    
+}
