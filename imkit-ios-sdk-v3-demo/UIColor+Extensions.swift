a8db2b89fe4bc199018241e5a90066eb9ececf11
sean <shonshon7@gmail.com>
2021-07-11 17:58:25 +0800

    [add] UIColor+extensions

---
+++ imkit-ios-sdk-v3-demo/UIColor+Extensions.swift
@@ -0,0 +1,34 @@
+//
+//  UIColor+Extensions.swift
+//  imkit-ios-sdk-v3-demo
+//
+//  Created by Sean on 2021/7/11.
+//  Copyright © 2021 Howard Sun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    convenience init?(hexString hex: String) {
+        var cString: String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if cString.hasPrefix("#") {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        guard cString.count == 6 else {
+            return nil
+        }
+        
+        var rgbValue: UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+        
+        self.init(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
